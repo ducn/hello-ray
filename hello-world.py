44a698ee8eb887c816a5018cc5206057cc8b45a6
root <root@VCINPC0033.localdomain>
2021-04-09 08:32:02 +0700

    update address

--- hello-world.py
+++ hello-world.py
@@ -2,7 +2,7 @@ import ray
 from ray import serve
 
 # Connect to the running Ray cluster.
-ray.init(address="auto")
+ray.init(address="auto", _redis_password='5241590000000000')
 # Bind on 0.0.0.0 to expose the HTTP server on external IPs.
 serve.start(detached=True, http_options={"host": "0.0.0.0"})
 
